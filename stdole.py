26ed27b64c52c7140797c376d538273b6e9872f4
jantoeee <jayo@xs4all.nl>
2025-01-18 14:14:39 +0100

    update html voor msi

---
+++ stdole.py
@@ -0,0 +1,33 @@
+import comtypes.gen._00020430_0000_0000_C000_000000000046_0_2_0 as __wrapper_module__
+from comtypes.gen._00020430_0000_0000_C000_000000000046_0_2_0 import (
+    VgaColor, OLE_YPOS_PIXELS, IFontEventsDisp, typelib_path, Font,
+    VARIANT_BOOL, Monochrome, IEnumVARIANT, IPictureDisp,
+    OLE_YSIZE_CONTAINER, FONTITALIC, Default, OLE_XSIZE_HIMETRIC,
+    Color, OLE_TRISTATE, CoClass, COMMETHOD, DISPPROPERTY,
+    FONTUNDERSCORE, IDispatch, IPicture, OLE_XPOS_PIXELS, GUID,
+    EXCEPINFO, _lcid, OLE_ENABLEDEFAULTBOOL, OLE_XSIZE_PIXELS,
+    OLE_YPOS_CONTAINER, OLE_XPOS_HIMETRIC, StdPicture, Gray,
+    _check_version, OLE_CANCELBOOL, OLE_YSIZE_HIMETRIC, dispid, IFont,
+    OLE_HANDLE, OLE_OPTEXCLUSIVE, FONTSTRIKETHROUGH, Unchecked,
+    Library, HRESULT, FONTBOLD, Checked, IFontDisp, Picture, StdFont,
+    BSTR, LoadPictureConstants, FontEvents, FONTNAME,
+    OLE_YSIZE_PIXELS, OLE_YPOS_HIMETRIC, OLE_XPOS_CONTAINER, FONTSIZE,
+    OLE_XSIZE_CONTAINER, DISPMETHOD, DISPPARAMS, OLE_COLOR, IUnknown
+)
+
+
+__all__ = [
+    'OLE_CANCELBOOL', 'OLE_YSIZE_HIMETRIC', 'VgaColor',
+    'OLE_YPOS_PIXELS', 'IFont', 'OLE_HANDLE', 'OLE_OPTEXCLUSIVE',
+    'FONTSTRIKETHROUGH', 'IFontEventsDisp', 'typelib_path', 'Font',
+    'Unchecked', 'Library', 'Monochrome', 'IPictureDisp',
+    'OLE_YSIZE_CONTAINER', 'FONTITALIC', 'Default', 'FONTBOLD',
+    'OLE_XSIZE_HIMETRIC', 'Color', 'Checked', 'OLE_TRISTATE',
+    'IFontDisp', 'Picture', 'StdFont', 'LoadPictureConstants',
+    'FontEvents', 'FONTNAME', 'OLE_YSIZE_PIXELS', 'OLE_YPOS_HIMETRIC',
+    'OLE_XPOS_CONTAINER', 'FONTUNDERSCORE', 'FONTSIZE',
+    'OLE_XSIZE_CONTAINER', 'OLE_COLOR', 'IPicture', 'OLE_XPOS_PIXELS',
+    'OLE_ENABLEDEFAULTBOOL', 'OLE_XSIZE_PIXELS', 'OLE_YPOS_CONTAINER',
+    'OLE_XPOS_HIMETRIC', 'StdPicture', 'Gray'
+]
+
